5bf475539c59ff5fddbe190c3a33a57110124c36
leavehouse <heavelouse@gmail.com>
2017-09-23 15:30:59 -0500

    Add a method that wraps the multipart request logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
+
+/*
+extern crate hyper;
 extern crate multipart;
 
+use hyper::client::Request;
+use hyper::method::Method;
+use hyper::net::Streaming;
+
+use multipart::client::Multipart;
+
+use std::io::Read;
+*/
+
+extern crate hyper;
+extern crate multipart;
+
+use hyper::client::Request;
+use hyper::method::Method;
 pub use multipart::client::Multipart;
+use std::io::{self, Read};
+
+pub enum RequestError {
+    ParseError(hyper::error::ParseError),
+    HyperError(hyper::Error),
+    IoError(io::Error),
+}
+
+impl From<hyper::error::ParseError> for RequestError {
+    fn from(e: hyper::error::ParseError) -> RequestError {
+        RequestError::ParseError(e)
+    }
+}
+
+impl From<hyper::Error> for RequestError {
+    fn from(e: hyper::Error) -> RequestError {
+        RequestError::HyperError(e)
+    }
+}
+
+impl From<io::Error> for RequestError {
+    fn from(e: io::Error) -> RequestError {
+        RequestError::IoError(e)
+    }
+}
+
+pub fn send_new_post_request<T>(url: T, mut data: &[u8]) -> Result<Vec<u8>, RequestError>
+                                where T: AsRef<str> {
+    let parsed_url = url.as_ref().parse()?;
+    let request = Request::new(Method::Post, parsed_url)?;
+    let mut mp_request = Multipart::from_request(request)?;
+    mp_request.write_stream("file", &mut data, None, None)?;
+    let mut response = mp_request.send()?;
+
+    if !response.status.is_success() {
+        let mut res = String::new();
+        response.read_to_string(&mut res).expect("failed to read response");
+        println!("response reported unsuccessful: {:?}\n {}", response, res);
+    }
+
+    let mut buf = vec![];
+    response.read_to_end(&mut buf)?;
+    Ok(buf)
+}
